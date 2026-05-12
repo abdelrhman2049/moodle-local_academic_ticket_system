00c10e165fde1dd007bde0eb6d80b5719b21ba3b
abdelrhman2049 <abdelrhman20499@gmail.com>
2026-05-12 17:56:01 +0000

    chore: bump version to 2026051200 and remove .idea from tracking

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_academic_ticket_system';
-$plugin->version = 2026012320;
+$plugin->version = 2026051200;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.0.0';
+$plugin->release = '3.1.0';
